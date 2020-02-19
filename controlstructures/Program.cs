a78a7e21f52a710c14b95bdd14617cab0c4cfd8a
Rohithapatchava <60814260+Rohithapatchava@users.noreply.github.com>
2020-02-19 17:36:13 +0530

    Initial commit

---
+++ controlstructures/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controlstructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the nunber");
+            int n = int.Parse(Console.ReadLine());
+            while(n<100)
+            {
+                Console.WriteLine("{0} is less than 100", n);
+                n = n + 10;
+            }
+        }
+    }
+}
